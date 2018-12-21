099a2c1096bf3f2dddeabde3092acf8c505fb787
Matías Olivera <matiasolivera@outlook.com>
2018-12-21 01:08:23 -0300

    Añadir estructura principal

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,63 @@
 <template>
-  <div></div>
+
+  <div>
+    <section class="section">
+      <div class="container">
+
+        <div class="columns">
+          <div class="column is-full has-text-centered">
+            <h1 class="title">
+              Conversión de divisas
+            </h1>
+            <p class="subtitle">
+              Dolares estadounidenses > divisa seleccionada
+            </p>
+          </div>
+        </div>
+
+        <div class="columns">
+          <div class="column is-half is-offset-one-quarter">
+            <div class="columns">
+
+              <div class="column">
+                <div class="field">
+                  <div class="control">
+                    <input
+                      class="input is-primary is-medium is-rounded"
+                      type="number"
+                      placeholder="100 U$D">
+                  </div>
+                </div>
+              </div>
+
+              <div class="column">
+                <div class="select is-medium is-rounded">
+                    <select name="divisas">
+                      <option value="">Seleccione una divisa...</option>
+                    </select>
+                </div>
+              </div>
+
+            </div>
+
+            <div class="box has-text-centered">
+              <p class="is-size-4 has-text-primary">Resultado</p>
+              <p class="is-size-5 has-text-grey-light">Descripción</p>
+            </div>
+            
+          </div>
+        </div>
+      </div>
+    </section>
+  </div>
+
 </template>
 
 <script>
+export default {
+  name: "app"
+};
 </script>
 
-<style>
+<style scoped>
 </style>
